f3e3f32ea5b1f0d3894e201ea0fb3b56a45dc9d4
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-09-01 15:11:20 +0300

    fragment

---
+++ app/src/main/java/com/german/crime/Programm2/CrimeActivity.java
@@ -0,0 +1,27 @@
+package com.german.crime.Programm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.german.crime.R;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
