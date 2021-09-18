316ea1a92ee58442aece846935f30f0f194aa23d
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-09-18 10:21:34 +0300

    recyclerView start

--- app/src/main/java/com/german/crime/Programm2/CrimeListFragment.java
+++ app/src/main/java/com/german/crime/Programm2/CrimeListFragment.java
@@ -1,6 +1,28 @@
 package com.german.crime.Programm2;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.german.crime.R;
 
 public class CrimeListFragment extends Fragment {
+
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container,
+                false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
