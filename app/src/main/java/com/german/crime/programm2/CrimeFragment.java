e89a3ec2f0d653b6e90171cdab7059887f592556
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-09-30 19:32:50 +0300

    dataPicker

--- app/src/main/java/com/german/crime/programm2/CrimeFragment.java
+++ app/src/main/java/com/german/crime/programm2/CrimeFragment.java
@@ -12,6 +12,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.german.crime.R;
 
@@ -20,6 +21,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -80,7 +82,15 @@ public class CrimeFragment extends Fragment {
         });
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+//        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
         return v;
     }
 }
--- app/src/main/java/com/german/crime/programm2/DatePickerFragment.java
+++ app/src/main/java/com/german/crime/programm2/DatePickerFragment.java
@@ -3,6 +3,8 @@ package com.german.crime.programm2;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -11,9 +13,8 @@ import com.german.crime.R;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
-                .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
-                .create();
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
     }
 }
