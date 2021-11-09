deee919075449a10e34198f0002b234daddeb97e
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-11-09 21:02:11 +0200

    add with 15

--- app/src/main/java/com/german/crime/programm2/Crime.java
+++ app/src/main/java/com/german/crime/programm2/Crime.java
@@ -8,10 +8,12 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime() {
         this(UUID.randomUUID());
     }
+
     public Crime(UUID id) {
         mId = id;
         mDate = new Date();
@@ -44,5 +46,13 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 }
